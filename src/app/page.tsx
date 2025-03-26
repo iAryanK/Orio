1fe5f9b97b8a792ce5d082e1950e26dfba435724
iAryanK <aryankinmail@gmail.com>
2025-03-27 00:11:06 +0530

    update: logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { ThemeToggle } from "@/components/shared/ThemeToggle";
-
+import Image from 'next/image';
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Image
+      src={"/logo.png"}
+      alt="logo"
+      width={48}
+      height={48}
+      className="animate-spin"
+      />
       <ThemeToggle />
     </div>
   );
